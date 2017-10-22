cd38dfa96061d5e3037de179e88be794170fcb72
Bradford Folkens <bfolkens@gmail.com>
2017-10-22 11:55:46 -0700

    vbump 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='MarketProfile',
-    version='0.1.1',
+    version='0.1.2',
     author='Brad Folkens',
     author_email='bfolkens@gmail.com',
     packages=find_packages(where="src"),
