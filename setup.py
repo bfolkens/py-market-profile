c894b7531770b682ce1a755d19f553071b493341
Bradford Folkens <bfolkens@gmail.com>
2018-09-30 20:17:22 -0700

    Added build/ dir to ignore, vbump to 0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='MarketProfile',
-    version='0.1.4',
+    version='0.1.5',
     author='Brad Folkens',
     author_email='bfolkens@gmail.com',
     packages=find_packages(where="src"),
