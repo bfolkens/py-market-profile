d7aff7cfe9f586f79977fb8dc5568339bb11a7bc
Bradford Folkens <i.am@bradfolkens.com>
2020-01-18 19:37:35 -0800

    Added scipy requirement to package

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     install_requires=[
         "numpy >= 1.13.0",
         "pandas >= 0.20.3",
+        "scipy >= 0.11.0"
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
